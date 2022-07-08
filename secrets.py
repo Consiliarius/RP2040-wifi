ac4a640b33598a6aeb34d84e7bd4a532ad70d903
Consiliarius <78850500+Consiliarius@users.noreply.github.com>
2022-07-08 19:24:40 +0100

    added wifi snippets

---
+++ secrets.py
@@ -0,0 +1,12 @@
+# Secret Keys.
+
+secrets = {
+    "ssid"                     : "h2g2-2g",                                    
+    "password"                 : "Hazel84Alex85Lily17",
+    "blynk_auth_token"         : "T0OXpXCGEk5mpCFeAcTZgsK4hwmFXZEE",
+    "thingspeak_write_api_key" : "my_thingspeak_write_api_key",
+    "telegram_bot_token"       : "my_telegram_bot_token",
+    "telegram_chat_id"         : "my_telegram_chat_id",
+    "aio_username"             : "my_adafruitio_username",
+    "aio_key"                  : "my_adafruitio_key"
+}
